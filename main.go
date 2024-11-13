bc3b6fb1643d7b3e6e21534dd46dfce3a4b70861
manhtai <vo@manhtai.com>
2024-11-13 14:47:22 +0700

    Done

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"embed"
+	"encoding/base64"
 	"fmt"
+	"html/template"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/jpeg"
-	"io"
+	"log"
 	"net/http"
 	"os"
+	"slices"
+	"strings"
 
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
@@ -16,7 +21,7 @@ import (
 )
 
 func loadFont(fontPath string, fontSize float64) (font.Face, error) {
-	fontBytes, err := os.ReadFile(fontPath)
+	fontBytes, err := tpl.ReadFile(fontPath)
 	if err != nil {
 		return nil, fmt.Errorf("could not read font file: %v", err)
 	}
@@ -61,16 +66,16 @@ func addLabel(img *image.RGBA, x, y int, label string, face font.Face) {
 	d.DrawString(label)
 }
 
-func generateInvite(templatePath, fontPath, text string, writer io.Writer) error {
+func generateInvite(templatePath, fontPath, text string) (*image.RGBA, error) {
 	file, err := os.Open(templatePath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer file.Close()
 
 	img, _, err := image.Decode(file)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Convert the image to RGBA format
@@ -81,7 +86,7 @@ func generateInvite(templatePath, fontPath, text string, writer io.Writer) error
 	fontSize := 32.0
 	face, err := loadFont(fontPath, fontSize)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer face.Close() // Remember to close the font face when done
 
@@ -91,18 +96,74 @@ func generateInvite(templatePath, fontPath, text string, writer io.Writer) error
 	// Add the text to the image
 	addLabel(rgba, x, y, text, face)
 
-	return jpeg.Encode(writer, rgba, nil)
+	return rgba, nil
+}
+
+//go:embed tpl/*
+var tpl embed.FS
+
+func serveImage(tp, text string, w http.ResponseWriter, r *http.Request) {
+	img, err := generateInvite(fmt.Sprintf("tpl/%s.jpg", tp), "tpl/font.ttf", text)
+	if err != nil {
+		log.Print(err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Cache-Control", "max-age=3600")
+	w.WriteHeader(http.StatusOK)
+	jpeg.Encode(w, img, nil)
+}
+
+type RenderData struct {
+	Text    string
+	Tpl     string
+	ImgHash string
+	Ok      bool
+	Host    string
 }
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /{tpl}/{text}", func(w http.ResponseWriter, r *http.Request) {
-		tpl := r.PathValue("tpl")
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFS(tpl, "tpl/index.html")
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		tp := r.URL.Query().Get("tpl")
+		text := r.URL.Query().Get("text")
+		hash := base64.URLEncoding.EncodeToString([]byte(fmt.Sprintf(`%s/%s`, tp, text)))
+		ok := slices.Contains([]string{"th", "vq"}, tp) && text != ""
+		tmpl.Execute(w, RenderData{ImgHash: hash, Text: text, Tpl: tp, Ok: ok})
+	})
+
+	router.HandleFunc("GET /t/{code}", func(w http.ResponseWriter, r *http.Request) {
+		code := r.PathValue("code")
+		data, err := base64.URLEncoding.DecodeString(code)
+		if err != nil {
+			log.Print(err)
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+
+		paths := strings.Split(string(data), "/")
+		if len(paths) < 2 {
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+
+		tp, text := paths[0], paths[1]
+		log.Print("tp", tp, "text", text)
+		serveImage(tp, text, w, r)
+	})
+
+	router.HandleFunc("GET /to/{tp}/{text}", func(w http.ResponseWriter, r *http.Request) {
+		tp := r.PathValue("tp")
 		text := r.PathValue("text")
-		w.Header().Set("Content-Type", "image/jpeg")
-		w.Header().Set("Cache-Control", "max-age=3600")
-		w.WriteHeader(http.StatusOK)
-		generateInvite(fmt.Sprintf("./%s.jpg", tpl), "./PlaywriteHRLijeva-VariableFont_wght.ttf", text, w)
+		serveImage(tp, text, w, r)
 	})
 
 	http.ListenAndServe(":3001", router)
